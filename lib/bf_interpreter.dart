69ccdb97f3e658f456823af80622aa07a7a588c4
JinGyeong Jeong <jjgjoojis@gmail.com>
2016-09-03 18:44:01 +0900

    Set directory layout.

--- lib/bf_interpreter.dart
+++ lib/bf_interpreter.dart
@@ -112,24 +112,3 @@ class Program {
     }
   }
 }
-
-main () async {
-  var stream = new File("test1.bf").openRead();
-  StringBuffer buf = new StringBuffer();
-  await for (var data in stream) {
-    for (var charCode in data) {
-      if (isCodeUnitOperator(charCode)) {
-        buf.writeCharCode(charCode);
-      }
-    }
-  }
-  var prog = new Program(buf.toString());
-  try {
-    await for (var code in prog.run()) {
-      stdout.write(new String.fromCharCode(code));
-    }
-  } catch (e) {
-    print('\nerror: $e');
-  }
-  // print(InstructionType.values[0]);
-}
