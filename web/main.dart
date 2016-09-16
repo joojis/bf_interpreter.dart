6774862f7c23e28e40f34d5eb604318515a9d7f7
JinGyeong Jeong <jjgjoojis@gmail.com>
2016-09-16 16:34:22 +0900

    Implement initial version of static web page.

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,28 @@
 import 'dart:html';
+import 'package:bf_interpreter/bf_interpreter.dart';
+
+TextAreaElement programInput, stdinInput, stdoutInput, stderrInput;
+
+final programCharactersWithEscape = r"+-<>.,[]".replaceAllMapped(new RegExp('.'), (match) => '\\${match.group(0)}');
+
+runProgram() async {
+  var prog = new Program(programInput.value.replaceAll(new RegExp('[^${programCharactersWithEscape}]'), ''));
+  var outBuffer = new StringBuffer();
+  await for (var ch in prog.run()) {
+    outBuffer.writeCharCode(ch);
+  }
+  stdoutInput.value = outBuffer.toString();
+}
 
 void main() {
-  querySelector('body').text = 'wowowowow!!!';
+  programInput = querySelector('textarea#program');
+  stdinInput = querySelector('textarea#stdin');
+  stdoutInput = querySelector('textarea#stdout');
+  stderrInput = querySelector('textarea#stderr');
+
+  querySelector('#executeButton').addEventListener('click', (e) {
+    print('click event: ${e}!');
+    print('program: ${programInput.value}');
+    runProgram();
+  });
 }
