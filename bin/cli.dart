b99d93b4f0b7c7c938de0581bb5f36f2d5955ae2
JinGyeong Jeong <jjgjoojis@gmail.com>
2016-09-23 06:17:06 +0900

    Implement bin/cli.dart

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,6 +1,30 @@
 import 'dart:io';
 import 'package:bf_interpreter/bf_interpreter.dart';
 
-main () async {
-  throw new UnimplementedError('Receive BF filepath through args and run here.');
+String _getDefaultFilepath() {
+  final segments = Platform.script.pathSegments;
+  return (segments.sublist(0, segments.length - 2)
+      ..addAll(['test', 'bf_testcases', 'hello_world_1.bf']))
+      .join('/');
+}
+
+main (List<String> arguments) async {
+  final file = new File(arguments.length > 0 ? arguments[0] : _getDefaultFilepath());
+  if (!file.existsSync()) {
+    print('File not exists: ${file.absolute.path}');
+    return;
+  }
+
+  final program = new Program(file.readAsStringSync());
+  print('---------- Program Start    ----------');
+  try {
+    await for (final ch in program.run()) {
+      stdout.writeCharCode(ch);
+    }
+  } finally {
+    print('---------- Program Finished ----------');
+    await for (final log in program.getLogStream()) {
+      print('Program Info: $log');
+    }
+  }
 }
